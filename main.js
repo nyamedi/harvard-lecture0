b2057558883f94cfbd1812435cccbb598acb2f08
Samuel Quayson <51247406+nyamedi@users.noreply.github.com>
2021-08-14 06:30:51 +0000

    Update main.js

--- main.js
+++ main.js
@@ -16,4 +16,4 @@ function showSlides(n, no) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  x[slideIndex[no]-1].style.display = "block";
+  x[slideIndex[no]-1].style.display = "block"; }
